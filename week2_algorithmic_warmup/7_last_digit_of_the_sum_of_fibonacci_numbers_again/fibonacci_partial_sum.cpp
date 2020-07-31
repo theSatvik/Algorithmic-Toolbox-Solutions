0b58928787182511638c0ede29055b230fc7962b
Satvik Shrivas <44926681+theSatvik@users.noreply.github.com>
2020-08-01 02:54:10 +0530

    Update fibonacci_partial_sum.cpp

--- week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
+++ week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
@@ -1,64 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 using std::vector;
 
-int get_fibonacci_mod_small(int n, int m)
-{
-    int fib[n + 1];
-    fib[0] = 0;
-    fib[1] = 1;
-    for (int i = 2; i <= n; i++)
-    {
-        fib[i] = (fib[i - 1] + fib[i - 2]) % m;
-    }
+int fibonacci_sum(long long n, long long m) {
+    if (n <= 1)
+        return n;
 
-    return fib[n];
-}
-
-int get_pisano_period_length(int m)
-{
-    if (m == 1)
-    {
-        return 0;
-    }
-    int first = -1;
-    int second = -1;
-    int i = 2;
-    while (1)
-    {
-        second = get_fibonacci_mod_small(i, m);
-        if (second == 1 && first == 0)
-        {
-            return i - 1;
+    vector<long long int> a;
+    a.push_back(0);
+    a.push_back(1);
+    long long int x,t;
+    long long i=2;
+    while(1){
+        x=(a[i-1]+a[i-2])%m;
+        a.push_back(x);
+        if(a[i]==1&&a[i-1]==0){
+            time_period=a.size()-2;
+            break;
         }
-        first = second;
-        i += 1;
+        i++;
     }
+    return a[n%time_period]%m;
 }
 
-long long get_fibonacci_huge_fast(long long n, long long m)
-{
-    int pisano_length = get_pisano_period_length(m);
-    long long rem;
-    do
-    {
-        rem = n % pisano_length;
-        n = rem;
-    } while (rem >= pisano_length);
-
-    return get_fibonacci_mod_small(n, m);
-}
-
-int fibonacci_sum_fast(long long from, long long to)
-{
-    return ((get_fibonacci_huge_fast(to + 2, 10) - 1) - (get_fibonacci_huge_fast(from + 1, 10) - 1));
-}
-
-int main()
-{
-    long long from, to;
-    std::cin >> from >> to;
-    int sum = fibonacci_sum_fast(from, to);
-    int value = sum < 0 ? (sum + 10) : sum;
-    std::cout << value;
+int main() {
+    long long m,n;
+    std::cin >> m >> n;
+    long long sum = std::abs(fibonacci_sum(n+2,10)-fibonacci_sum(m+1,10)+10);
+    std::cout << sum%10 << '\n';
 }
